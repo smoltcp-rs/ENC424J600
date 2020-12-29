15b08c780e51da5d407ed652e9b86cbc9d150451
Harry Ho <hh@m-labs.hk>
2020-12-29 17:39:03 +0800

    nix/itm-tools.nix: Fix hash for nixpkgs 20.09

--- nix/itm-tools.nix
+++ nix/itm-tools.nix
@@ -1,19 +1,21 @@
 { stdenv, fetchFromGitHub, rustPlatform, pkg-config }:
 
 rustPlatform.buildRustPackage rec {
-  version = "2019-11-15";
   pname = "itm-tools";
+  version = "unstable-2019-11-15";
 
   src = fetchFromGitHub {
     owner = "japaric";
-    repo = "itm-tools";
+    repo = pname;
     rev = "e94155e44019d893ac8e6dab51cc282d344ab700";
     sha256 = "19xkjym0i7y52cfhvis49c59nzvgw4906cd8bkz8ka38mbgfqgiy";
   };
 
   cargoPatches = [ ./itm-tools-cargo-lock.patch ];
 
-  cargoSha256 = "0is702s14pgvd5i2m8aaw3zcsshqrwj97mjgg3wikbc627pagzg7";
+  # This hash is computed under nixpkgs 20.09.
+  # Older versions nixpkgs would require a different value.
+  cargoSha256 = "0rl2ph5igwjl7rwpwcf6afnxly5av7cd6va6wn82lxm606giyq75";
 
   nativeBuildInputs = [ pkg-config ];
 
